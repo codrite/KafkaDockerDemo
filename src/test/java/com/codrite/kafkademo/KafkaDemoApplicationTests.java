2c70fe8fe9f1ce3150de268710244053146edfa4
Arnabh Chakravarty <arnab@codrite.com>
2021-01-28 20:52:53 -0500

    Improvements

--- src/test/java/com/codrite/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/com/codrite/kafkademo/KafkaDemoApplicationTests.java
@@ -1,12 +1,10 @@
 package com.codrite.kafkademo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KafkaDemoApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
